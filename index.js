514231546595bdf4407c1be00371b89da0cb8dd6
vobichngoc26 <ngocvb234111e@st.uel.edu.vn>
2026-03-12 10:39:00 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const app=express()
-const port=3000
-app.get("/",(req,res)=>{
-    res.send("Hello Resful API")
-})
-app.listen(port,()=>{
-    console.log(`My Server listening on port ${port}`)
-})
